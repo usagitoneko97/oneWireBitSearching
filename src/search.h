68c8b3db791e79e77113a08fb9ce7bd2b138818d
Usagitoneko <hogouxian@gmail.com>
2017-07-03 17:07:03 +0800

    created oneWireSearch project

---
+++ src/search.h
@@ -0,0 +1,5 @@
+#ifndef _SEARCH_H
+#define _SEARCH_H
+
+
+#endif // _SEARCH_H
---
+++ test/test_search.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "search.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_search_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement search");
+}
